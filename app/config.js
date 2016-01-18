613df8b031fc142d249936167f759f57b2696079
Tyler Hall <0tillathehun0@gmail.com>
2016-01-18 00:01:30 -0500

    added firebase reference object for use in app. Set up cron job configuration to call each included parser

--- app/config.js
+++ app/config.js
@@ -1,7 +1,23 @@
-var config = module.exports;
+var Firebase = require('firebase');
+var CronJob = require('cron').CronJob;
+var moment = require('moment');
 
+function parse(){
+    //require all parsers here and call there parse method
+    require("app/n-charleston/parser").parse();
+    config.lastCronJob = moment().format();
+    console.log("Cron Job successfully ran at: ", config.lastCronJob);
+}
+
+new CronJob('10 * * * * *', parse, null, true, 'America/Los_Angeles');
+
+var config = {};
 
 config.express = {
     port: process.env.PORT || 3000,
     ip: "127.0.0.1"
 };
+
+config.firebaseRef = new Firebase("https://auction-scraper.firebaseio.com/");
+
+module.exports = config;
